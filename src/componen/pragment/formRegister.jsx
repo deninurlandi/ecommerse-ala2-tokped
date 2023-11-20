37844cbcc0fd012d57be5403c63d4d9e92846e0e
deninurlandi <badjolex14@gmail.com>
2023-11-20 19:02:50 +0700

    mengganti path

--- src/componen/pragment/formRegister.jsx
+++ src/componen/pragment/formRegister.jsx
@@ -18,7 +18,7 @@ export default function FormRegister() {
     localStorage.setItem('telp', telp);
 
     if (password === confirmpassword) {
-      window.location.href = '/login';
+      window.location.href = 'https://store-ala2-tokped.vercel.app/login';
     } else {
       setTimeout(() => {
         setValid(true);
